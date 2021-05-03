fc20957ae5255790cff128322e8011b1fdfa78a7
zhanghuan <1120410457@qq.com>
2021-05-03 22:05:39 +0800

    部署测试

--- pages/layout/index.vue
+++ pages/layout/index.vue
@@ -10,7 +10,7 @@
         <nav class="navbar navbar-light">
             <div class="container">
                 <!-- <a class="navbar-brand" href="index.html">conduit</a> -->
-                <nuxt-link class="navbar-brand" to="/" exact>conduit</nuxt-link>
+                <nuxt-link class="navbar-brand" to="/" exact>conduit 部署测试</nuxt-link>
                 <ul class="nav navbar-nav pull-xs-right">
                     <li class="nav-item">
                         <!-- Add "active" class when you're on that page" -->
